4efac4a359e49d0c7c07bb09b701c14ea1a8dd9a
iglesiaskevinralph <iglesiaskevinralph@gmail.com>
2023-08-10 16:11:33 +0800

    test run time

--- src/App.js
+++ src/App.js
@@ -1,46 +1,9 @@
 import './App.css';
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import io from 'socket.io-client';
-import Home from './pages/Home';
-import Game from './pages/Game';
-import Admin from './pages/Admin';
 
-const socket = io.connect('https://crescendo-race-socket.onrender.com');
 
 function App() {
-  const [username, setUsername] = useState('');
-  const room = 'crescendoRace';
 
-  return (
-    <Router>
-      <div className='App'>
-        <Routes>
-          <Route path='/' element={
-              <Home
-                username={username}
-                setUsername={setUsername}
-                room={room}
-                socket={socket}
-              />
-            }
-          />
-          <Route path='/game' element={
-              <Game
-                username={username}
-                room={room}
-                socket={socket}
-              />
-            }
-          />
-          <Route path='/admin' element={
-              <Admin room={room} socket={socket} />
-            }
-          />
-        </Routes>
-      </div>
-    </Router>
-  );
+  return <div>RUNNING</div>;
 }
 
 export default App;
